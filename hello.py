49a888cc9d2730aff947f563b5d5ca7e444b0ae6
pk-kanth <kanthchandra6300@gmail.com>
2024-12-24 18:23:47 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,3 +7,22 @@ sum = num1 + num2
 
 # printing values
 print("Sum of", num1, "and", num2 , "is", sum)
+
+
+
+
+
+
+
+# Define a lambda function to add two numbers
+add_numbers = lambda x, y: x + y
+
+# Take input from the user
+num1 = 1
+num2 = 2
+
+# Call the lambda function to add the two numbers
+result = add_numbers(num1, num2)
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", result)
