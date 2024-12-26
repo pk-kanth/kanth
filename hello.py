1fe2cc17b8307ce378e065b8621f234a8e022f79
pk-kanth <kanthchandra6300@gmail.com>
2024-12-26 12:10:11 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -26,3 +26,20 @@ result = add_numbers(num1, num2)
 
 # Print the result
 print("The sum of", num1, "and", num2, "is", result)
+
+
+#To define a function that take two integers 
+# and return the sum of those two numbers
+def add(a,b):
+  return a+b
+
+#initializing the variables
+num1 = 10
+num2 = 5
+
+#function calling and store the result into sum_of_twonumbers
+sum_of_twonumbers = add(num1,num2)
+
+#To print the result
+print("Sum of {0} and {1} is {2};" .format(num1,num2, sum_of_twonumbers))
+                           
